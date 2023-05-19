ddab2ae0b9dd511cd975c3b4082a28c2b810f305
Kishan Kumar <kishan.kumar@zimtive.com>
2023-05-19 09:06:38 +0530

    updated

--- server.js
+++ server.js
@@ -18,12 +18,17 @@ function get_random(array) {
    return array[Math.floor(Math.random() * array.length)]; 
 }
 
-if (process.env.NODE_ENV === 'production') {
-    //*Set static folder up in production
-    app.use(express.static('frontend/build'));
+app.use(express.static(path.join(__dirname , 'frontend' , 'build')));
+app.get('/', (request,response) => {
+    response.sendFile(path.join(__dirname , 'frontend' , 'build' , 'index.html'));
+});
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build','index.html')));
-  }
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname , 'frontend' , 'build')));
+    app.get('/', (request,response) => {
+        response.sendFile(path.join(__dirname , 'frontend' , 'build' , 'index.html'));
+    });
+}
 
 io.on("connection", (socket) => {
   socket.on("admin connected with server", (adminName) => {
